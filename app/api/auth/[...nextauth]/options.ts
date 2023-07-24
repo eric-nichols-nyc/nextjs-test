d76f06911cf2532f310d03d8091c0580f5f8d174
Eric Nichols <ebn646@gmail.com>
2023-07-24 19:41:25 -0400

    added api folder

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,47 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username:',
+          type: 'text',
+          placeholder: 'Eric',
+        },
+        password: {
+          label: 'Password:',
+          type: 'password',
+          placeholder: 'nextauth',
+        },
+      },
+      async authorize(credentials) {
+        // This is where you need to retrieve user data
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: '42', name: 'Eric', password: 'nextauth' };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
